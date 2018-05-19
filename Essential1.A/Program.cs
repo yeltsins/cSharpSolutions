520976149eed5bfc8c7a44806301d0b938146c00
Сергей Ельцин <yeltsin.sv@gmail.com>
2018-05-19 23:47:11 +0500

    Add project files.

---
+++ Essential1.A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential1.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
