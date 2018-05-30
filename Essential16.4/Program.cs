a1ad87f17d4be86c19b53e4cfa4d6d3bda307766
Sergey Yeltsin <yeltsin.sv@gmail.com>
2018-05-30 19:39:02 +0500

    Problems 16.4, 17.2

---
+++ Essential16.4/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential16._4
+{
+
+    class Date
+    {
+        public int Day
+        {
+            get => day;
+            set
+            {
+                if (value<=0)
+                {
+                    throw new Exception("Day cant be <=0");
+                }
+                if (value > 31)
+                {
+                    throw new Exception("Day cant be >31");
+                }
+                day = value;
+            }
+        }
+        private int day;
+        private int month;
+        public int Month
+        {
+            get => month;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Month cant be <=0");
+                }
+                if (value > 12)
+                {
+                    throw new Exception("Month cant be >12");
+                }
+                month = value;
+            }
+        }
+
+        public int Year { get; set; }
+        public Date(int day,int month,int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public static int operator -(Date date1,Date date2)
+        {
+
+            TimeSpan timeSpan = new DateTime(date1.Year, date1.Month, date1.Day)
+                - new DateTime(date2.Year, date2.Month, date2.Day);
+
+            return timeSpan.Days;
+        }
+
+        public static Date operator +(Date date,int days)
+        {
+            DateTime dateTime = new DateTime(date.Year, date.Month, date.Day).AddDays(days);
+            return new Date(dateTime.Day, dateTime.Month, dateTime.Year);
+        }
+
+        public override string ToString()
+        {
+            return $"Year: {Year}, Month: {Month}, Day: {Day}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Date date1 = new Date(32, 5, 2018);
+                Date date2 = new Date(10, 6, 2018);
+                Console.WriteLine(date1 + 11);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Essential17.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential17._2
+{
+    class Program
+    {
+
+
+
+        class Car
+        {
+            public string Company { get; }
+            public string Model { get; }
+            public int Year { get; }
+            public string Color { get; }
+            public Car(string company,string model,int year,string color)
+            {
+                Company = company;
+                Model = model;
+                Year = year;
+                Color = color;
+            }
+            public override string ToString()
+            {
+                return $"Company: {Company}, Model: {Model}, Year: {Year}, Color: {Color}";
+            }
+        }
+
+        class Customer
+        {
+            public string Model { get; }
+            public string Name { get; }
+            public string Tel { get; }
+            public Customer(string model,string name,string tel)
+            {
+                Model = model;
+                Name = name;
+                Tel = tel;
+            }
+            public override string ToString()
+            {
+                return $"Model: {Model}, Name: {Name}, Tel: {Tel}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+            { new Car("Mers","S600",1999,"Red"),
+            new Car("Mers","S601",1998,"Green")};
+
+            List<Customer> customers = new List<Customer>
+            { new Customer("S600","Serg","999"),
+            new Customer("S601","Alex","600")};
+
+            var customer = customers.Where(a => a.Name == "Serg")
+                .Join(cars, a => a.Model, a => a.Model, (cust, car) => (Model: cust.Model, Color: car.Color, Name: cust.Name, Year: car.Year));
+            //var car = cars.Where(a => a.Model == customer.Model).First();
+            Console.WriteLine(customer.First());
+            //Console.WriteLine(car);
+            Console.Read();
+
+        }
+    }
+}
