e22863ef61062ba2f6d00e33a4b96b94ac680501
Sergey Yeltsin <yeltsin.sv@gmail.com>
2018-05-31 19:27:20 +0500

    Solution for lesson 17

---
+++ Essential17.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential17._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] dictionary = {
+                new { Eng = "House", Rus = "Дом" },
+                new { Eng = "Tree", Rus = "Дерево" },
+                new { Eng = "Window", Rus = "Окно" }
+            };
+
+            var dict = new Dictionary<dynamic, dynamic>
+            {
+                { new {Key="1"}, new {Val="One" } },
+                { new {Key="2"}, new {Val="Two" } }
+            };
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key.Key+ " - "+item.Value.Val);
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ Essential17.A/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential17.A
+{
+
+    static class Calculator
+    {
+        public static dynamic Add(dynamic A, dynamic B)
+        {
+            double valA = 0;
+            double valB = 0;
+            
+            if (double.TryParse(Convert.ToString(B), out valB) &&
+                double.TryParse(Convert.ToString(A), out valA))
+            {
+                return valA + valB;
+            }
+            return "NaN";
+        }
+        public static dynamic Sub(dynamic A, dynamic B)
+        {
+            double valA = 0;
+            double valB = 0;
+
+            if (double.TryParse(Convert.ToString(B), out valB) &&
+                double.TryParse(Convert.ToString(A), out valA))
+            {
+                return valA - valB;
+            }
+            return "NaN";
+        }
+        public static dynamic Mult(dynamic A, dynamic B)
+        {
+            double valA = 0;
+            double valB = 0;
+
+            if (double.TryParse(Convert.ToString(B), out valB) &&
+                double.TryParse(Convert.ToString(A), out valA))
+            {
+                return valA * valB;
+            }
+            return "NaN";
+        }
+        public static dynamic Div(dynamic A, dynamic B)
+        {
+            double valA=0;
+            double valB = 0;
+            if (double.TryParse(Convert.ToString(B),out valB)&&
+                double.TryParse(Convert.ToString(A), out valA))
+            {
+                if (valB!=0)
+                {
+                    return valA / valB;
+                }                
+            }
+            return "NaN";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(1.5f,"6,7"));
+
+
+            Console.Read();
+        }
+    }
+}
